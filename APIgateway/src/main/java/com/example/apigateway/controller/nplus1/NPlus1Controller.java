466abd18baf175049411a5c4c0efedbd7c12363c
jelena-pesevski <jpesevski99@gmail.com>
2023-06-19 15:07:40 +0200

    n+1 problem solved using DataLoader

--- APIgateway/src/main/java/com/example/apigateway/controller/nplus1/NPlus1Controller.java
+++ APIgateway/src/main/java/com/example/apigateway/controller/nplus1/NPlus1Controller.java
@@ -3,22 +3,35 @@ package com.example.apigateway.controller.nplus1;
 import com.example.apigateway.feign.NPlus1Client;
 import com.example.apigateway.model.nplus1.Book;
 import com.example.apigateway.model.nplus1.Rating;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.graphql.data.method.annotation.BatchMapping;
+import org.dataloader.DataLoader;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.execution.BatchLoaderRegistry;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Controller
-@RequiredArgsConstructor
 public class NPlus1Controller {
 
     private final NPlus1Client client;
+
+    public NPlus1Controller(NPlus1Client client, BatchLoaderRegistry batchLoaderRegistry) {
+        this.client = client;
+
+        batchLoaderRegistry.forTypePair(Integer.class, List.class)
+                .registerMappedBatchLoader((booksIds, env) -> {
+                    Map map = executeCall(booksIds);
+                    return Mono.just(map);
+                });
+    }
 
     @QueryMapping
     public List<Book> books() {
@@ -26,20 +39,38 @@ public class NPlus1Controller {
         return client.getAllBooks();
     }
 
+    //@SchemaMapping which solves n+1 problem using DataLoader
+    @SchemaMapping
+    public CompletableFuture<List<Rating>> ratings(Book book, DataLoader<Integer, List<Rating>> loader) {
+        return loader.load(book.id());
+    }
+
+    private Map<Integer, List<Rating>> executeCall(Set<Integer> booksIds) {
+        log.info("Fetching ratings for books with ids: {}", booksIds);
+
+        var response = client.getAllRatingsByBookIds(List.copyOf(booksIds));
+
+        return response.getRatingsByBookIds();
+    }
+
+    //@SchemaMapping which causes n+1 problem
 //    @SchemaMapping
 //    public List<Rating> ratings(Book book) {
 //        log.info("Fetching rating for book, id {}", book.id());
 //        return client.getRatingsByBookId(book.id());
 //    }
 
-    @BatchMapping
-    public Map<Book, List<Rating>> ratings(List<Book> books) {
-        log.info("Fetching ratings for all books");
-
-        var response = client.getAllRatingsByBookIds(books.stream().map(Book::id).toList());
-
-        return mapBookIdsToBooks(response.getRatingsByBookIds(), books);
-    }
+    //@BatchMapping solves n+1 problem
+//    @BatchMapping
+//    public Map<Book, List<Rating>> ratings(List<Book> books) {
+//        List<Integer> booksIds = books.stream().map(Book::id).toList();
+//
+//        log.info("Fetching ratings for books with ids: {}", booksIds);
+//
+//        var response = client.getAllRatingsByBookIds(booksIds);
+//
+//        return mapBookIdsToBooks(response.getRatingsByBookIds(), books);
+//    }
 
     private Map<Book, List<Rating>> mapBookIdsToBooks(Map<Integer, List<Rating>> ratingsForBooks, List<Book> books) {
         Map<Book, List<Rating>> map = new HashMap<>();
