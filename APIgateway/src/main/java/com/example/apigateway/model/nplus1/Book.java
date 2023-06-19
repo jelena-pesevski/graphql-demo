41c5110e287792b29d408b493ec98f7e02753fdc
jelena-pesevski <jpesevski99@gmail.com>
2023-06-19 14:18:00 +0200

    Added example with BatchMapping, solved n+1 problem without DataLoader

--- APIgateway/src/main/java/com/example/apigateway/model/nplus1/Book.java
+++ APIgateway/src/main/java/com/example/apigateway/model/nplus1/Book.java
@@ -1,6 +1,4 @@
 package com.example.apigateway.model.nplus1;
 
-import lombok.Data;
-
 public record Book(Integer id, String name, String author, String publisher, Double price) {
 }
--- APIgateway/src/main/java/com/example/apigateway/model/nplus1/Rating.java
+++ APIgateway/src/main/java/com/example/apigateway/model/nplus1/Rating.java
@@ -1,6 +1,4 @@
 package com.example.apigateway.model.nplus1;
 
-import lombok.Data;
-
 public record Rating(Integer id, Integer bookId, Integer rating, String comment, String user) {
 }
